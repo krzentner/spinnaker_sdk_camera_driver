a1cec76b2df067c904b236573aa3fabf7dbabf27
K.R. Zentner <krzentner@gmail.com>
2021-03-31 20:22:42 -0700

    Remove logname patching

--- download_and_install_spinnaker.sh
+++ download_and_install_spinnaker.sh
@@ -15,11 +15,6 @@ wget https://coe.northeastern.edu/fieldrobotics/spinnaker_sdk_archive/spinnaker-
 tar -zxvf spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH-pkg.tar.gz
 cd spinnaker-$SPINNAKER_VERSION-$SPINNAKER_LINUX_ARCH
 
-# fix issue with 'logname' command on docker, required by spinnaker 2.2.0.48
-mv /usr/bin/logname /usr/bin/logname_old
-echo "echo root" > /usr/bin/logname
-chmod +x /usr/bin/logname
-
 # auto accept spinnaker license agreements
 echo libspinnaker libspinnaker/present-flir-eula note '' | debconf-set-selections
 echo libspinnaker libspinnaker/accepted-flir-eula boolean true | debconf-set-selections
